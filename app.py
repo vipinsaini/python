fbe4b96ad3438b57c8184617242dafeb594f3609
Vipin Saini <sainivipin@johndeere.com>
2019-09-26 22:46:49 -0500

    add docker

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 name = input("Enter your name: ")
 age = input("Enter your age: ")
-result = int(name) + int(age)
+result = name + str(age)
 print(result)
 print("Hello " + name + "! Your age is " + age)
